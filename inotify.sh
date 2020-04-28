dd680d95af2639389329f55dededf78635877dac
Francisco Treviño <zerofrancisco@gmail.com>
2020-04-28 12:08:24 -0500

    monitor a dir or file modifications done on it
    bash script to monitor a dir or file modifications done on it

---
+++ inotify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#franciscotv
+#script to monitor a dir or file modifications done on it
+#write, change permissions, open, close ...
+#using inotify gnu/linux api function
+#for loop runs to wait for the creation of the file/dir if it doesn't exist yet,
+#once is found, the inotifywait wait starts tracking for changes
+
+for i in `seq 1 100000`;
+do
+	while inotifywait  /rootFolder/hereIsMyDirToMonitor; do
+		echo "put here the actions you want to do when the dir/file is modified"
+		ls -al /someDir
+		touch /someFile
+	done
+sleep 2
+done
+
